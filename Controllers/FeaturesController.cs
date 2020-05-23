5adca865d9fa9a1c217ee437d91447656bfc55ac
ldrahan <l.u.demchyk@gmail.com>
2020-05-23 19:46:46 +0300

    added api/features endpoint

---
+++ Controllers/FeaturesController.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Vega.Controllers.Resources;
+
+namespace Vega.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        private readonly VegaDbContext vegaDbContext;
+        private readonly IMapper mapper;
+
+        public FeaturesController(VegaDbContext vegaDbContext, IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.vegaDbContext = vegaDbContext;
+        }
+
+        [HttpGet("api/features")]
+        public IEnumerable<FeatureResource> GetFeatures()
+        {
+            var features = vegaDbContext.Features;
+            return mapper.Map<IEnumerable<FeatureResource>>(features);
+        }
+    }
+}
